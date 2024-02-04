c04d2271c68c8275a8b947b0717a38234b5527a1
zay <isaac@kenyon.co.uk>
2024-02-04 02:34:50 +0000

    📝 Patreon Crossposting Message

--- src/client/patreon.ts
+++ src/client/patreon.ts
@@ -1,6 +1,8 @@
 import { error } from "itty-router";
-import type { Env } from "../index";
+import { RouteBases, Routes, Snowflake } from 'discord-api-types/v10';
 import { isPatreonRequest } from "./verify";
+import type { Env } from "../index";
+
 
 export async function handlePatreonRequest(
 	request: Request,
@@ -9,6 +11,18 @@ export async function handlePatreonRequest(
 	const { content, isValid } = await isPatreonRequest(request, env);
 
 	if (!content || !isValid) return error(401);
+
+	const postTitle = 'Patreon: ' + content.data.attributes.title as String
+	const postLink = 'https://www.patreon.com' + content.data.attributes.url as String
+
+	const channelId = "1196157901954355242" as Snowflake
+	await fetch(`${RouteBases.api}${Routes.channelMessages(channelId)}`, {
+		method: 'POST',
+		headers: { Authorization: `Bot ${env.DISCORD_TOKEN}`, 'content-type': 'application/json' },
+		body: JSON.stringify({
+			content: `${postTitle}\n${postLink}`
+		})
+	})
 
 	return error(200);
 }
